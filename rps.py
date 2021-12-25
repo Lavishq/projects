748cd63afde6b6ae34d5b8e016f8cc8fcc945358
Lavishq <96654752+Lavishq@users.noreply.github.com>
2021-12-25 18:57:10 +0530

    initial commit
    This is rock paper scissor game
    10 games 
    and result

---
+++ rps.py
@@ -0,0 +1,83 @@
+"""
+rock paper scisir game
+play 10 times and gives result with wins, draws and loses
+also it does not count wrong inputs
+"""
+import random
+elements = ["rock", "paper", "scisir"]
+your_score = 0
+cpu_score = 0
+draw_g = 0
+total_games = 0
+game_limit = 10
+def ri():
+    r = random.choice(elements)
+    return r
+
+def lost():
+    print("You lost this round")
+
+def won():
+    print("You won this round")
+
+def draw():
+    print("This round was draw")
+    
+def cpu_choice():
+    print(f"cpu choose {cpu}")
+
+while total_games < game_limit:
+    print("")
+    choice = input("choose r, p, s as rock, paper, scisir:").lower()
+    cpu=ri()
+
+    if choice == 'r':
+        if elements[1]==cpu:
+            lost()
+            cpu_choice()
+            your_score += 1
+        elif elements[0]==cpu:
+            draw()
+            cpu_choice()
+            draw_g += 1
+        else:
+            won()
+            cpu_choice()
+            cpu_score +=1
+
+    elif choice == 'p':
+        if elements[2]==cpu:
+            lost()
+            cpu_choice()
+            your_score += 1
+        elif elements[1]==cpu:
+            draw()
+            cpu_choice()
+            draw_g += 1
+        else:
+            won()
+            cpu_choice()
+            cpu_score +=1
+            
+    elif choice == 's':
+        if elements[0]==cpu:
+            lost()
+            cpu_choice()
+            your_score += 1
+        elif elements[1]==cpu:
+            won()
+            cpu_choice()
+            cpu_score +=1
+        else:
+            draw()
+            cpu_choice()
+            draw_g += 1
+    
+    else:
+        print('Wrong input')
+        continue
+    total_games += 1
+
+if total_games == game_limit:
+    print()
+    print(f"You won {your_score} times and lost {cpu_score} times and the game was draw {draw_g} times")
